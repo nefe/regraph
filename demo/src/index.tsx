41b80699bb2680b9a61b2df6ba5726fd2b4709d8
PerkinJ <perkin1221@gmail.com>
2020-02-20 17:40:41 +0800

    Updates

--- demo/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { render } from "react-dom";
-
-import EditorDemo from './EditorDemo';
-
-const App:React.FC = ()=> <EditorDemo />
-
-render(<App />, document.getElementById("main"));
