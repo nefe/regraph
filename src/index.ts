0e1347a4e88a6ee1b8112761588d23509d548c6b
PerkinJ <perkin1221@gmail.com>
2020-03-13 00:24:26 +0800

    deps: update version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import ReScreen from './ReScreen'; 
-import { DAGAIU as DAG, GraphLayout, GroupGraphLayout } from './BaseLayout';
+import { DAGAIU as DAG, GraphLayout, GroupGraphLayout,Bubble } from './BaseLayout';
 import Tree from './BaseGraph/Tree';
 import Graph from './BaseGraph/Graph';
 import { Point, crossProduct, crossPoint, isCross, distance, getControlPoint, quadratic, vectorAngle } from './Utils/graph';
@@ -19,6 +19,7 @@ const BaseLayout = {
   DAG,
   GraphLayout,
   GroupGraphLayout,
+  Bubble,
 };
 
 const BaseGraph = {
